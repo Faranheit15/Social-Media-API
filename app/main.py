7f4086731eb885e6179213431367cc4a528332bb
Faranheit15 <ffaranm15@gmail.com>
2024-08-28 01:39:05 +0530

    POST request using SQLAlchemy complete

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel  # schema
 from typing import Optional
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from time import sleep
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, SessionLocal, get_db
+
+models.Base.metadata.create_all(bind=engine)  # this line creates the table
 
 app = FastAPI()  # initializing the FastAPI instance
 
@@ -12,35 +17,20 @@ app = FastAPI()  # initializing the FastAPI instance
 class Post(BaseModel):  # define the schema for our POST Body
     title: str
     content: str
-    published: bool = True
-    rating: Optional[int] = None
+    # published: bool = True
+    # rating: Optional[int] = None
 
 
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="social-media",
-                                user="postgres", password="postgres", cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful", cursor)
-        break
-    except Exception as error:
-        print("Error while connecting to database", error)
-        sleep(2)
-
-my_posts = [{"id": 1, "title": "Post 1", "content": "This is post 1"}, {"id": 2, "title": "Post 2",
-                                                                        "content": "This is post 2"}, {"id": 3, "title": "Post 3", "content": "This is post 3"}]
-
-
-def find_post(id):
-    for post in my_posts:
-        if post["id"] == id:
-            return post
-
-
-def find_index_post(id):
-    for index, post in enumerate(my_posts):
-        if post["id"] == id:
-            return index
+# while True:
+#     try:
+#         conn = psycopg2.connect(host="localhost", database="social-media",
+#                                 user="postgres", password="postgres", cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successful", cursor)
+#         break
+#     except Exception as error:
+#         print("Error while connecting to database", error)
+#         sleep(2)
 
 # decorator. This contains the FastAPI instance and the path to be accessed. It can either be called an API route or Path Operation.
 
@@ -51,22 +41,34 @@ async def root():  # function. The async keywork is optional, it is used to indi
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # using SQL
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    # using SQLAlchemy
+    posts = db.query(models.Post).all()
     return {"status": "success", "status_code": status.HTTP_200_OK, "error": False,  "message": "These are your posts", "data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
+def create_post(post: Post, db: Session = Depends(get_db)):
     # Here we will be using Pydantic to define a schema for our payload. This will restrict the user to follow the schema and he cannot send any garbage value in the POST Body.
     # post_dict = post.dict()
     # post_dict["id"] = len(my_posts) + 1
     # my_posts.append(post_dict)
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+    # using SQL
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    # using SQLAlchemy
+    # new_post = models.Post(
+    #     title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"status": "success", "status_code": status.HTTP_201_CREATED, "error": False,  "message": "New post created successfully", "data": new_post}
 
 
