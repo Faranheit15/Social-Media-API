3bd4bd5638f2dd6ee77c72b2617386b70b724232
Faranheit15 <ffaranm15@gmail.com>
2024-08-29 23:56:06 +0530

    hashing complete

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from time import sleep
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, SessionLocal, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -67,8 +67,22 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/user", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password - user.password
+    user.password = utils.hash(user.password)
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user/{id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with ID {id} not found")
+
+    return user
